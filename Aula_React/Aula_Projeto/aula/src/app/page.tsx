6461eab3674dfbf9e9e2010b6a5c8a95e67e6fd7
Marcelo Figueira <marcelosousafigueira@gmail.com>
2023-09-16 19:56:29 -0300

    .
    .

---
+++ Aula_React/Aula_Projeto/aula/src/app/page.tsx
@@ -0,0 +1,15 @@
+import {Circle,Square} from "@/components/Geo";
+
+
+
+const Page = () => {
+  return(
+    <div>
+      <h1>ola mundo </h1>
+      <Square/>
+      <Circle />
+    </div>
+    
+  );
+}
+export default Page;
---
+++ Aula_React/Aula_Projeto/aula/src/components/Geo.tsx
@@ -0,0 +1,17 @@
+export const Square = () => {
+    return(
+        <div className="w-52 h-52 bg-orange-700 text-white">
+            texto area 
+        </div>
+    );
+}
+
+export const Circle = () => {
+    return (
+        <div className="w-52 h-52 bg-orange-500 text-white rounded-full">
+            circulo
+        </div>
+    );
+}
+
+
